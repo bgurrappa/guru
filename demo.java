2d637e84f772e023a300daed7dab2f516b176bfd
bgurrappa <borravallagurrappa@gmail.com>
2019-01-27 12:55:22 +0000

    newcode3

--- demo.java
+++ demo.java
@@ -4,17 +4,20 @@ class demo
 p.s.void main(........)
 {
 (.....................)
+}
+}
+}
+class demo1 extends demo
+{
+p.s.void main(.........)
+{
+(.............)
+
 
 
 }
 
 
-}
 
 
-
-
-
-
-
-}
+} 
